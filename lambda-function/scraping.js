df041bc3793a51fdb4f95844ea35750a7a93a5ce
Lídia Sarti <lid.sarti@gmail.com>
2024-04-23 00:25:46 -0300

    ♻️ refactor: melhorando a performance do scrapping

--- lambda-function/scraping.js
+++ lambda-function/scraping.js
@@ -1,11 +1,14 @@
 const chromium = require("@sparticuz/chromium");
 const puppeteer = require("puppeteer-core");
-const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBDocumentClient, PutCommand, BatchWriteCommand } = require("@aws-sdk/lib-dynamodb");
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const Responses = require('./api-responses');
 
 module.exports.handler = async () => {
+    let browser = null;
+
     try {
-        const browser = await puppeteer.launch({
+        browser = await puppeteer.launch({
             args: chromium.args,
             defaultViewport: chromium.defaultViewport,
             executablePath: await chromium.executablePath(),
@@ -16,9 +19,8 @@ module.exports.handler = async () => {
         await page.goto("https://www.mercadolivre.com.br/ofertas#nav-header");
         
         const productElements = await page.$$('.promotion-item');
-        const products = [];
-
-        await Promise.all(productElements.map(async (product) => {
+        
+        const productsPromises = productElements.slice(0, 3).map(async (product, index) => {
             const nameElement = await product.$('.promotion-item__title');
             const name = nameElement ? await nameElement.evaluate(element => element.textContent.trim()) : 'Title not found';
 
@@ -28,51 +30,61 @@ module.exports.handler = async () => {
             const prices = price.split('R$');
             price = `R$${prices[1]}`;
 
-            products.push({ name, price });
-        }));
+            return { name, price };
+        });
 
-        await browser.close();
+        const products = await Promise.all(productsPromises);
 
-        const client = new DynamoDBClient({});
-        const docClient = new DynamoDBDocumentClient(client);
-        
-        if (products && products.length > 0) {
-            await Promise.all(products.map(async (product, index) => {
-                try {
-                    await docClient.send(
-                        new PutCommand({
-                          TableName: 'ProdutosMercadoLivre',
-                          Item: {
-                            id: index,
-                            name: product.name,
-                            price: product.price,
-                          },
-                        })
-                      );
-                } catch (error) {
-                    return {
-                        statusCode: 500,
-                        message: `Error inserting product ${index}: ${error}`
-                    };
-                }
-            }));
-        } else {
-            console.error('No elements found.');
-            return {
-                statusCode: 500,
-                message: 'No elements found.'
-            };
-        }
-        
-        return {
-            statusCode: 200,
-            message: 'Products insertion successful.'
-        };
+        await insertProductsIntoDynamoDB(products);
+
+        return Responses._200({ message: `Made the inclusion of the following products in the database`, products });
 
     } catch (error) {
-        return {
-            statusCode: 500,
-            message: `Failed to fetch products from Mercado Livre ${error}`
-        };
+        return Responses._500({ message: `Failed to fetch products from Mercado Livre ${error}`});
+    } finally {
+        if(browser != null)
+        {
+            await browser.close();
+        }
     }
 };
+
+async function insertProductsIntoDynamoDB(products) {
+    const client = new DynamoDBClient({});
+    const docClient = new DynamoDBDocumentClient(client);
+
+    const batchSize = 25;
+    const chunks = chunkArray(products, batchSize);
+
+    for (const chunk of chunks) {
+        try {
+            const putRequests = chunk.map((product, index) => ({
+                PutRequest: {
+                    Item: {
+                        id: index,
+                        name: product.name ,
+                        price: product.price,
+                    }
+                }
+            }));
+
+            await docClient.send(
+                new BatchWriteCommand({
+                    RequestItems: {
+                        'ProdutosMercadoLivre': putRequests
+                    }
+                })
+            );
+        } catch (error) {
+            console.error(`Error inserting batch of products: ${error}`);
+        }
+    }
+}
+
+function chunkArray(array, size) {
+    const chunkedArray = [];
+    for (let i = 0; i < array.length; i += size) {
+        chunkedArray.push(array.slice(i, i + size));
+    }
+    return chunkedArray;
+}
